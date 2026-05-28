8c968108263c37248ec648943fc15384659d4507
adithyan s kumar <adithyans1438@gmail.com>
2026-05-28 21:32:45 +0530

    fix : scroll to top resolve issue

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
-import ScrollToTop from "../components/scrollToTop/ScrollToTOp";
+import ScrollToTop from "../components/scrollToTop/ScrollToTop";
 import BackToTop from "../components/backToTop/BackToTop";
 
 export default function MainLayout() {
